c165e54b1a8d7d26dd0c487d7d159e04c0ecb4a6
Mourid Chaimaa <chaimaa.mourid1@gmail.com>
2023-05-28 17:13:01 -0700

    modified:   _printf.c 	modified:   get_flags.c 	new file:   get_precision.c 	new file:   get_size.c 	new file:   get_width.c 	new file:   handle_print.c 	new file:   utils.c 	new file:   write_handlers.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format
- * by omnia and chaimaa
+ * by oumnia and chaimaa
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
